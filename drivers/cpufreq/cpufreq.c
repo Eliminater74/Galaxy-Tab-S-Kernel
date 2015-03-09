67ab411263c5ac61c46a274568e74739bb2456a6
UpInTheAir <upintheair.xda@gmail.com>
2015-03-09 16:22:27 +0700

    Samsung DYNAMIC Hotplug control: hotplug_cpu_up_boost_value 30 > 0
    This effectively enables all cores online and a snapier UI. Users can increase via sysfs for possible better battery savings.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -86,7 +86,7 @@ extern void hlpr_set_gpu_gov_cpu_table(int gpu_table[]);
 #ifdef CONFIG_EXYNOS5_DYNAMIC_CPU_HOTPLUG
 static unsigned int hotplug_enabled_flag = 1;
 static unsigned int hotplug_cpu_up_load_value = 4;
-static unsigned int hotplug_cpu_up_boost_value = 30;
+static unsigned int hotplug_cpu_up_boost_value = 0;
 static unsigned int normalmin_freq_value = 250000;
 static unsigned int hotplug_cpu_down_hysteresis_value = 20;
 #endif
